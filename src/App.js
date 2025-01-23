27dd9349e5d7c53a4b63c08ed06be005f21f3f1f
Sahil Amberkar <sahilamberkar2003@gmail.com>
2025-01-23 11:27:35 +0530

    Day1 Completed

--- src/App.js
+++ src/App.js
@@ -3,17 +3,9 @@ import React from "react";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const handleClick = () => {
-    alert("Button clicked");
-  };
-
   return (
     <>
       <Navbar />
-      <div className="flex flex-col items-center justify-center h-screen">
-        Hello World
-        <button onClick={handleClick}>Click ME</button>
-      </div>
     </>
   );
 }
